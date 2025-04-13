3839d832b8716cdfaad74c698b944e4df2a0d9b8
Laura <sofirgallego@gmail.com>
2025-04-13 11:17:28 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function cargarPagina(pagina) {
+    fetch(pagina)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar la página');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('contenido').innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));        
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    cargarPagina("../pages/home.html");
+});
